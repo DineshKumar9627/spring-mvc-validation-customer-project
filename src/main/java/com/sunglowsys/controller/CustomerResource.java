1dcb2396d670c370c7832fd96bb9a7ecc72c00d5
Dinesh Kumar <dineshzz600@outlook.com>
2022-02-18 14:12:12 +0530

    modify resource and config

--- src/main/java/com/sunglowsys/controller/CustomerResource.java
+++ src/main/java/com/sunglowsys/controller/CustomerResource.java
@@ -2,7 +2,8 @@ package com.sunglowsys.controller;
 
 import com.sunglowsys.domain.Customer;
 import com.sunglowsys.service.CustomerService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,37 +12,49 @@ import java.util.List;
 @RequestMapping("/api")
 public class CustomerResource {
 
-    @Autowired
-    private CustomerService customerService;
+    private  final Logger log = LoggerFactory.getLogger(CustomerResource.class);
+
+    private  final CustomerService customerService;
+
+    public CustomerResource(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
 
     @PostMapping("/customer")
     public Customer create(@RequestBody Customer customer){
+        log.debug("Rest request to create customer: {}",customer);
         return customerService.save(customer);
     }
 
     @PostMapping("/customers")
     public List<Customer> saveAll(@RequestBody List<Customer> customers){
+        log.debug("Rest request to create all customer: {}",customers);
         return customerService.saveAll(customers);
     }
 
     @GetMapping("/customer/{id}")
     public Customer getById(@PathVariable ("id") Long id){
+        log.debug("Test request to find customer by id: {}",id);
         return customerService.findById(id);
 
     }
 
     @PutMapping("/customer/{id}")
     public Customer update(@RequestBody Customer customer,@PathVariable ("id") Long id){
+        log.debug("Rest request to update customer: {}",id);
         return  customerService.update(customer,id);
     }
 
     @DeleteMapping("/customer/{id}")
      public  void delete(@PathVariable("id") Long id){
+        log.debug("Rest request to  delete customer:  {}",id);
         customerService.delete(id);
      }
 
      @GetMapping("/customer")
      public List<Customer> getAll(){
+        log.debug("Rest request to find all customers :{}");
         List<Customer> customers = customerService.findAll();
         return customers;
      }
